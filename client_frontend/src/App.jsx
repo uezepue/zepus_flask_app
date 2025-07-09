419fef035d36f6b281fa7c33e9944d70c58f838a
Uchenna Ezepue <uezepue2@yahoo.com>
2025-07-10 00:57:19 +0100

    Deploy working admin and client builds

--- client_frontend/src/App.jsx
+++ client_frontend/src/App.jsx
@@ -1,6 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Home from './pages/Home';
+import Login from './pages/Login'; // Add other pages you want to test
 
 export default function App() {
-  return <Home />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        {/* Add more routes as needed */}
+      </Routes>
+    </Router>
+  );
 }
