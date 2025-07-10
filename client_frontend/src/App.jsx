b0457d776b9f66850f8847ec24f0c66fda89f601
Uchenna Ezepue <uezepue2@yahoo.com>
2025-07-10 01:39:36 +0100

    Deploy working admin and client builds

--- client_frontend/src/App.jsx
+++ client_frontend/src/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import Home from './pages/Home';
 import Login from './pages/Login';
+import VerifyDocuments from './pages/VerifyDocuments';
 
 export default function App() {
   return <Home />;
   return <Login />;
+  return <VerifyDocuments />;
 }
