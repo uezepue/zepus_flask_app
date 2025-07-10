16f4318c8a18bd2eb4b4a7e15b213d0b6de73a9d
Uchenna Ezepue <uezepue2@yahoo.com>
2025-07-10 06:50:56 +0100

    Deploy working admin and client builds

--- client_frontend/src/App.jsx
+++ client_frontend/src/App.jsx
@@ -3,10 +3,12 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import VerifyDocuments from './pages/VerifyDocuments';
 import DoctorChatRoom from './pages/DoctorChatRoom';
+import PatientChatRoom from './pages/PatientChatRoom';
 
 export default function App() {
   return <Home />;
   return <Login />;
   return <VerifyDocuments />;
   return <DoctorChatRoom />;
+  return <PatientChatRoom />;
 }
